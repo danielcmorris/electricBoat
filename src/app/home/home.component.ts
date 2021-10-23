a3af32bb100c9a7cfebbfe3329b389f04375ac44
Daniel Morris <dmorris@morrisdev.com>
2021-10-22 17:57:57 -0700

    added performance factors

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { MonoTypeOperatorFunction } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -18,7 +19,7 @@ export class HomeComponent implements OnInit {
 
 
 
-  motor = {
+  motor: Motor = {
     makeModel: "Motenergy ME1616",
     maxVoltageRating: 80,
     rpmAtMaxVoltage: 8000,
@@ -27,7 +28,7 @@ export class HomeComponent implements OnInit {
     maxPeakCurrentRating: 0,
     maxPeakTorqueRating: 0,
     motorEfficiencyRating: .92,
-    motorTorqueConstantLbsPerFtPerAmp: 0.148,
+    motorTorqueConstantLbsPerFtPerAmp: 0.1475124299,
     motorVoltageConstantRpmsPerVolt: 100.00,
     motorshaftToPropShaftReductionRatio: 2.00
   }
@@ -41,7 +42,7 @@ export class HomeComponent implements OnInit {
     maxContinuousCurrentRating: 0,
   }
 
-  battery = {
+  battery:Battery = {
     makeModel: "Chevy Bolt 10S",
     bci: "N/A",
     ampHourRating: 173,
@@ -62,19 +63,19 @@ export class HomeComponent implements OnInit {
     this.propeller = new Propeller();
     this.propeller.diameter = 14.00;
     this.propeller.rpmAtHullSpeed = 1300;
-    this.propeller.maxHullSpeedInKnots = 6.57909    ;
-   
+    this.propeller.maxHullSpeedInKnots = 6.57909;
 
-    let speeds = [.3,.4,.5,.6,.7,.8,.9,.95,1,1.05, 1.1,1.15,1.2,1.25,1.3,1.35]
 
-    speeds.forEach(s  => {
+    let speeds = [.3, .4, .5, .6, .7, .8, .9, .95, 1, 1.05, 1.1, 1.15, 1.2, 1.25, 1.3, 1.35]
+
+    speeds.forEach(s => {
       let speed = this.speed(s);
-      var p = new Performer(speed, this.propeller);
+      var p = new Performer(speed, this.propeller, this.motor, this.battery);
 
       this.performers.push(p);
     });
 
-     
+
 
 
 
@@ -97,7 +98,7 @@ export class HomeComponent implements OnInit {
       d = parseFloat(`${this.depth}`);
       var pi;
       if (d > 0)
-        this.csa = Math.PI * (d ^ 2) / 2;
+        this.csa = Math.PI * Math.pow(d, 2) / 2;
     } catch {
       this.csa = 0;
     }
@@ -175,7 +176,22 @@ export class HomeComponent implements OnInit {
 
 }
 
+export class Motor {
 
+  makeModel: string = '';
+  maxVoltageRating: number = 0;
+  rpmAtMaxVoltage: number = 0;
+  maxContinuousCurrentRating: number = 0;
+  maxContinuousTorqueRating: number = 0;
+  maxPeakCurrentRating: number = 0;
+  maxPeakTorqueRating: number = 0;
+  motorEfficiencyRating: number = 0;
+  motorTorqueConstantLbsPerFtPerAmp: number = 0;
+  motorVoltageConstantRpmsPerVolt: number = 0;
+  motorshaftToPropShaftReductionRatio: number = 0;
+
+
+}
 export class Propeller {
   diameter: number = 0;
   rpmAtHullSpeed: number = 0;
@@ -193,8 +209,20 @@ export class Propeller {
 
 
 }
+
+
+export class Battery {
+  makeModel: string = "";
+  bci: any ="";
+  ampHourRating: number = 0;
+  voltage: number = 0;
+  count: number = 0;
+  stringVoltage: number = 0;
+  peukertNumber: number = 0;
+}
+
 export class Performer {
-  strSpeed :string ="";
+  strSpeed: string = "";
   speed: number = 0;
   propeller: Propeller;
   public get rpm(): number {
@@ -203,11 +231,100 @@ export class Performer {
   }
 
   public get hp(): number {
-    let h = ((((this.rpm ^ 0.6) / 632.7) * this.propeller.diameter ^ (1 / 0.2)))
-    return h;
+
+    let y = Math.pow(((Math.pow(this.rpm, 0.6) / 632.7) * this.propeller.diameter), (1 / 0.2))
+
+    return y;
   }
 
-  constructor(speed: number, prop: Propeller) {
+  public get torque(): number {
+    let t = (this.hp * 5252) / this.rpm
+    return t;
+  }
+
+  public get thrust(): number {
+    //=(((D37*$B$22/12)^0.67)*62.72)
+    let t = (Math.pow((this.speed * this.rpm / 12), 0.67) * 62.72)
+    return t
+  }
+
+
+  public get motorTorque(): number {
+    //=E37/$H$28
+    let t = this.torque / this.motor.motorshaftToPropShaftReductionRatio
+    return t
+  }
+
+  public get motorRpm(): number {
+    let mr = this.rpm * this.motor.motorshaftToPropShaftReductionRatio;
+    return mr;
+  }
+
+
+  public get volts(): number {
+    let v = this.motorRpm / this.motor.motorVoltageConstantRpmsPerVolt;
+    return v
+  }
+
+
+  public get current(): number {
+
+    //=(G37/$H$26)/($H$25-0.04)
+    let c = (this.motorTorque / this.motor.motorTorqueConstantLbsPerFtPerAmp) / (this.motor.motorEfficiencyRating - 0.04);
+    return c
+
+  }
+
+  public get kw(): number {
+    let k = this.volts * this.current / 1000
+    return k;
+  }
+
+
+  public get motorHp(): number {
+    let mhp = this.kw * 1000 / 746
+    return mhp;
+  }
+
+
+  public get stringCurrent(): number {
+    let sv = this.kw * 1000/this.battery.stringVoltage;
+    return sv;
+  }
+
+  public get batteryCurrent() : number{
+    //=M37/(($O$30*$O$29)/$O$31)
+    let bc = this.stringCurrent/((this.battery.count*this.battery.voltage)/this.battery.stringVoltage)
+    return bc;
+  }
+
+
+
+  public get runTime50():number{
+    let rt50 =((this.battery.ampHourRating/Math.pow(this.batteryCurrent,this.battery.peukertNumber))*0.5)*60;
+    return rt50;
+
+  }
+  public get runTime80():number{
+    let rt50 =((this.battery.ampHourRating/Math.pow(this.batteryCurrent,this.battery.peukertNumber))*0.8)*60;
+    return rt50;
+
+  }
+  
+  public get runTimeHour():number{
+    let rt50 =((this.battery.ampHourRating/Math.pow(this.batteryCurrent,this.battery.peukertNumber)));
+    return rt50;
+
+  } 
+   public get runTimeFull():number{
+    let rt50 =((this.battery.ampHourRating/Math.pow(this.batteryCurrent,this.battery.peukertNumber)))*60;
+    return rt50;
+
+  }
+
+
+
+  constructor(speed: number, prop: Propeller, private motor: Motor, private battery: Battery) {
     this.speed = speed;
     this.strSpeed = speed.toFixed(1);
     this.propeller = prop;
