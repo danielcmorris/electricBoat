4cca2497e61089c623db08b0df9fb18e85f9374c
Daniel Morris <dmorris@morrisdev.com>
2021-10-25 09:38:23 -0700

    cleaning out unused modules

--- src/app/app-common.module.ts
+++ src/app/app-common.module.ts
@@ -1,57 +1,48 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { RouterModule } from "@angular/router";
-
-
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import { MatListModule } from '@angular/material/list';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatOptionModule } from '@angular/material/core';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatToolbarModule } from '@angular/material/toolbar';
-
 import {MatTabsModule} from '@angular/material/tabs';
-
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatRadioModule } from '@angular/material/radio';
-import { MatExpansionModule } from '@angular/material/expansion'
-import { MatProgressBarModule, MatProgressBar } from '@angular/material/progress-bar';
+
+// import { RouterModule } from "@angular/router";
+// import { MatSidenavModule } from '@angular/material/sidenav';
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatListModule } from '@angular/material/list';
+// import { MatCheckboxModule } from '@angular/material/checkbox';
+// import { MatMenuModule } from '@angular/material/menu';
+// import { MatSelectModule } from '@angular/material/select';
+// import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+// import { MatGridListModule } from '@angular/material/grid-list';
+// import { MatToolbarModule } from '@angular/material/toolbar';
+// import { MatTooltipModule } from '@angular/material/tooltip';
+// import { MatOptionModule } from '@angular/material/core';
+// import { MatRadioModule } from '@angular/material/radio';
+// import { MatExpansionModule } from '@angular/material/expansion'
+// import { MatProgressBarModule, MatProgressBar } from '@angular/material/progress-bar';
 
 @NgModule({
   imports: [
+  
+  
+  
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    MatCardModule, MatCheckboxModule, MatListModule, MatButtonModule,MatTabsModule,
-   
-  
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
- 
-    MatSelectModule,
-    MatMenuModule,
-    MatSnackBarModule,  //this is basically toaster popups
-  
-    MatInputModule,
     MatFormFieldModule,
+    MatInputModule,
+    MatCardModule, 
+    MatButtonModule,
+    MatTabsModule,
     BrowserAnimationsModule,
     
   ],
-  declarations: [
-
-  ],
+  declarations: [ ],
   providers: [],
   exports: [ 
     CommonModule,
@@ -62,7 +53,6 @@ import { MatProgressBarModule, MatProgressBar } from '@angular/material/progress
     MatCardModule,
     MatButtonModule,
     MatTabsModule,
- 
     BrowserAnimationsModule],
   entryComponents: []
 })
