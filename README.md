03fb61d68a4f43570525a74b7ec4f3cee979671e
Daniel Morris <dmorris@morrisdev.com>
2021-10-25 09:20:17 -0700

    build fixes and cleanup with tabs

--- README.md
+++ README.md
@@ -12,3 +12,16 @@ initially found here: [Thunderstruck EV](http://www.thunderstruck-ev.com/Manuals
 
 I'd like to simply make some of these formulas a bit more user friendly and perhaps support the community a bit.  I'll be adding in some references to my own boat and the vendors who have provided me with support over the transition from diesel to electric.
  
+## Build Information
+
+I host in a subdirectory of my website here:  http://morrisdev.com/eboat.  To build this, I changed the angular.json file to include "baseHref": "./eboat/", in the productoin configuration after file-replacements happen.
+the htaccess file looks like this:
+
+RewriteEngine On
+RewriteBase /myapp/
+Options +FollowSymLinks
+
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteRule ^(.*)$ index.html [L,QSA]
+
+I included it in the build on angular.json as well.
--- src/app/app-common.module.ts
+++ src/app/app-common.module.ts
@@ -17,79 +17,52 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatExpansionModule } from '@angular/material/expansion'
-import { MatProgressBarModule, MatProgressBar } from '@angular/material/progress-bar';
+
+import {MatTabsModule} from '@angular/material/tabs';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatRadioModule } from '@angular/material/radio';
+import { MatExpansionModule } from '@angular/material/expansion'
+import { MatProgressBarModule, MatProgressBar } from '@angular/material/progress-bar';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    MatCardModule, MatCheckboxModule, MatListModule, MatButtonModule,
-    MatSelectModule,
-    MatRadioModule, MatExpansionModule, MatProgressBarModule,
-
-    //  BrowserModule,
-    RouterModule,
-    // FlexLayoutModule,
+    MatCardModule, MatCheckboxModule, MatListModule, MatButtonModule,MatTabsModule,
+   
+  
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    MatTooltipModule,
-    // MatOptionModule,
+ 
     MatSelectModule,
     MatMenuModule,
     MatSnackBarModule,  //this is basically toaster popups
-    MatGridListModule,
-    MatToolbarModule,
-    // MatButtonModule,
-    // MatRadioModule,
-    // MatCheckboxModule,
-    MatCardModule,
-    // MatProgressBarModule,
-    MatExpansionModule,
-    MatProgressBarModule,
+  
     MatInputModule,
     MatFormFieldModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    
   ],
   declarations: [
 
   ],
   providers: [],
-  exports: [ CommonModule,
+  exports: [ 
+    CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    MatFormFieldModule, MatInputModule,
-    MatCardModule, MatCheckboxModule, MatListModule, MatButtonModule,
-    MatSelectModule,
-    MatRadioModule, MatExpansionModule, MatProgressBarModule,
-
-    //  BrowserModule,
-    RouterModule,
-    // FlexLayoutModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatTooltipModule,
-    // MatOptionModule,
-    MatSelectModule,
-    MatMenuModule,
-    MatSnackBarModule,  //this is basically toaster popups
-    MatGridListModule,
-    MatToolbarModule,
-    // MatButtonModule,
-    // MatRadioModule,
-    // MatCheckboxModule,
+    MatFormFieldModule, 
+    MatInputModule,
     MatCardModule,
-    // MatProgressBarModule,
-    MatExpansionModule,
-    MatProgressBarModule,
+    MatButtonModule,
+    MatTabsModule,
+ 
     BrowserAnimationsModule],
   entryComponents: []
 })
